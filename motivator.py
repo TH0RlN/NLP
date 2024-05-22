f99adcb2f7bb91b98e6085ce694c8d7c161c9b1c
TH0RlN <aprariasperez@gmail.com>
2024-05-22 09:25:52 +0200

    feat(motivator.py): added option to change depth with argv

--- motivator.py
+++ motivator.py
@@ -1,8 +1,18 @@
+from sys import argv
 from nltk import CFG
 from nltk.parse.generate import generate
 from random import choice
 
 def main():
+    depth = 0
+    if len(argv) == 2:
+        depth = int(argv[1])
+    elif len(argv) == 1:
+        depth = 5
+    else:
+        print("Usage: python motivator.py [depth]")
+        exit(1)
+
     grammar = CFG.fromstring("""
         S -> NP VP
 
@@ -17,12 +27,13 @@ def main():
         PP -> P NP
         P -> "in" | "through" | "with"
     """)
-
-    parser = generate(grammar, depth=4)
+    
+    parser = generate(grammar, depth=depth)
     phrases = []
     for sentence in parser:
         phrases.append(' '.join(sentence))
     print(choice(phrases))
+    exit(0)
 
 if __name__ == "__main__":
     main()
